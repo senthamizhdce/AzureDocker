ec7462b9414c9a8b2e4404f20d86ed2ecea557c1
senthamizhdce <senthamizh.lcs@gmail.com>
2019-10-31 20:00:11 +0530

    Create Microsoft Graph SDK project

---
+++ MicrosoftGraphSDK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicrosoftGraphSDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
